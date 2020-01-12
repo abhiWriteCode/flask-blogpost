2b6c720dd88ee7d11ef57149b1f7533f09ac5251
dark <abhigoswami2106@gmail.com>
2020-01-13 03:08:35 +0530

    bash file to run

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,15 @@
-source ../venv/bin/activate
+if [ ! -d ../venv ];
+then 
+	python3 -m venv ../venv
+	echo "virtaul env created"
+	source ../venv/bin/activate
+	pip install --upgrade pip
+else
+	source ../venv/bin/activate
+fi
+echo "virtaul env activated"
+
 FLASK_APP=flaskapp
 FLASK_ENV=development
+
 flask run
