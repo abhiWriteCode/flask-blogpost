f86df5d6ab718524ea8d8056a83aca4285618289
abhiWriteCode <abhigoswami2106@gmail.com>
2020-07-01 12:17:46 +0530

    PEP8 format

--- src/flaskapp/auth.py
+++ src/flaskapp/auth.py
@@ -1,11 +1,11 @@
 from functools import wraps
 
 from flask import (
-	Blueprint, flash, g, redirect, render_template, 
-	request, session, url_for
+    Blueprint, flash, g, redirect, render_template,
+    request, session, url_for
 )
 from werkzeug.security import (
-	check_password_hash, generate_password_hash
+    check_password_hash, generate_password_hash
 )
 
 from flaskapp.db import get_db
@@ -16,85 +16,85 @@ blueprint = Blueprint('auth', __name__, url_prefix='/auth')
 
 @blueprint.route('/register', methods=['GET', 'POST'])
 def register():
-	if request.method == 'POST':
-		username = request.form['username']
-		password = request.form['password']
-		db = get_db()
-		error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
 
-		if not username:
-			error = 'Username is required.'
-		elif not password:
-			error = 'Password is required.'
-		elif db.execute(
-			'SELECT id FROM user WHERE username = ?', (username,)
-		).fetchone() is not None:
-			error = 'User {} is already registered.'.format(username)
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+                'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
 
-		if error is None:
-			db.execute(
-				'INSERT INTO user (username, password) VALUES (?, ?)',
-				(username, generate_password_hash(password))
-			)
-			db.commit()
-			return redirect(url_for('auth.login'))
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
 
-		flash(error)
+        flash(error)
 
-	return render_template('auth/register.html')
+    return render_template('auth/register.html')
 
 
 @blueprint.route('/login', methods=['GET', 'POST'])
 def login():
-	if request.method == 'POST':
-		username = request.form['username']
-		password = request.form['password']
-		db = get_db()
-		error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
 
-		user = db.execute(
-			'SELECT * FROM user WHERE username = ?', (username,)
-		).fetchone()
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
 
-		if user is None:
-			error = 'Incorrect username'
-		elif not check_password_hash(user['password'], password):
-			error = 'Incorrect password'
+        if user is None:
+            error = 'Incorrect username'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password'
 
-		if error is None:
-			session.clear()
-			session['user_id'] = user['id']
-			return redirect(url_for('blog.index'))
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('blog.index'))
 
-		flash(error)
+        flash(error)
 
-	return render_template('auth/login.html')
+    return render_template('auth/login.html')
 
 
 @blueprint.before_app_request
 def load_logged_in_user():
-	user_id = session.get('user_id')
+    user_id = session.get('user_id')
 
-	if user_id is None:
-		g.user = None
-	else:
-		g.user = get_db().execute(
-			'SELECT * FROM user WHERE id = ?', (user_id,)
-		).fetchone()
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
 
 
 @blueprint.route('/logout')
 def logout():
-	session.clear()
-	return redirect(url_for('blog.index'))
+    session.clear()
+    return redirect(url_for('blog.index'))
 
 
 def login_required(f):
-	@wraps(f)
-	def wrapped_f(*args, **kwargs):
-		if g.user is None:
-			return redirect(url_for('auth.login'))
+    @wraps(f)
+    def wrapped_f(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
 
-		return f(*args, **kwargs)
+        return f(*args, **kwargs)
 
-	return wrapped_f
+    return wrapped_f
--- src/run.py
+++ src/run.py
@@ -2,9 +2,9 @@ from flaskapp import create_app
 
 if __name__ == '__main__':
 
-	app = create_app(test_config=None)
-	app.run(
-		host='127.0.0.1',
-		port=5000,
-		debug=True
-	)
+    app = create_app(test_config=None)
+    app.run(
+        host='127.0.0.1',
+        port=5000,
+        debug=True
+    )
